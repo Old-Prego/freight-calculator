d1b3147c6330edb066e1792d248647410225c9cb
Luke Perrine <lukep@voyager.local>
2021-06-04 13:47:00 -0500

    Got the CSV reading and formatting done, next step is to get the API call working in a for each value and try not to get rejected for DDOSing or whatever"

--- script.js
+++ script.js
@@ -2,16 +2,36 @@ const fetch = require("node-fetch");
 const converter = require("length-distance-converter");
 const csv = require('csv-parser');
 const fs = require('fs');
+const locations = [];
 
-function getDistance(Location) {
+function loadCSV() {
+    fs.createReadStream('IMPORT.csv')
+        .pipe(csv())
+        .on('data', (data) => locations.push(data))
+        .on('end', () => {
+            locations.forEach(function(element, index) {
+                var words = element.LOCATION.split(' ');
+                var loc = words.join('+');
+                locations[index] = loc;
+            })
 
-    var distanceSearch = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=315+6th+Ave+E+Alexandria+MN+56308&destinations=803+Central+Ave+N+Brandon+MN+56315&key=AIzaSyBsdC0E4_Rp_Jbl2m9wvO6O1xhHbxIRtlU";
+            getDistance(locations);
+        });
+}
 
+function getDistance(locations) {
+
+    // TODO: Add for each to loop through locations making calls for every index
+    // TODO: Then, add returns to array and write to the CSV file
+
+    var distanceSearch = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=" + locations + "&destinations=803+Central+Ave+N+Brandon+MN+56315&key=AIzaSyBsdC0E4_Rp_Jbl2m9wvO6O1xhHbxIRtlU";
+    console.log(distanceSearch);
     fetch(distanceSearch)
         .then(function(response) {
             return response.json();
         })
         .then(function(data) {
+            console.log(data);
             console.log(data.rows[0].elements[0].distance);
             var kilo = data.rows[0].elements[0].distance.value / 1000;
             console.log(kilo);
@@ -22,5 +42,6 @@ function getDistance(Location) {
         })
 };
 
-getDistance();
+// getDistance();
+loadCSV();
 
